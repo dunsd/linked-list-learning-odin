1708ca63aac1f1e830dcd3e4f89c74f8655a68ef
dunsd <dthomasparker@gmail.com>
2023-03-21 21:44:49 +0000

    Add append function and getlength

---
+++ index.js
@@ -0,0 +1,9 @@
+import linkedList from "./lists.js";
+
+let list = linkedList();
+
+list.append(5);
+list.append(6);
+console.log(list.getLength());
+
+
--- lists.js
+++ lists.js
@@ -1,5 +1,41 @@
-import node from "./node"
+import node from "./node.js"
 
 const linkedList = () => {
+    let head = null;
+    let length = 0;
 
+    function append(value) {
+        if(head === null){
+            head = node(value);
+        }
+        else{
+            let currentNode = head;
+            console.log(currentNode);
+            while(currentNode.next !== null){
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node(value);
+            console.log(currentNode);   
+            console.log(currentNode.next);   
+        }
+        console.log(length)
+        length+=1;
+    }
+
+    // function prepend(value) {
+        
+    // }
+
+    function getLength() {
+        return length;
+    }
+
+    return{
+        head,
+        append,
+        getLength,
+
+    }
 }
+
+export default linkedList;
